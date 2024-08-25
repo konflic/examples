9e5471cf8dd4f1b2d291da3247a9e9b20bb49e6d
konflic <konflic@mail.ru>
2024-08-25 13:49:52 +0300

    add full screeen to slideshow

--- games/cars_slides/index.js
+++ games/cars_slides/index.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+let show = document.getElementById("show");
 
 // Next/previous controls
 function plusSlides(n) {
@@ -14,15 +14,23 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "grid";
-    dots[slideIndex - 1].className += " active";
+}
+
+showSlides(slideIndex);
+
+
+function openFullscreen() {
+    if (show.requestFullscreen) {
+        show.requestFullscreen();
+    } else if (show.webkitRequestFullscreen) { /* Safari */
+        show.webkitRequestFullscreen();
+    } else if (show.msRequestFullscreen) { /* IE11 */
+        show.msRequestFullscreen();
+    }
 }
